41a587125f9528651f3b8d34620b8be7ec794630
Oron Feinerman <oronf@mda.org.il>
2023-03-05 19:02:28 +0200

    Add test

--- MagickClickOnce/Program.cs
+++ MagickClickOnce/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ImageMagick;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,24 @@ namespace MagickClickOnce
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Testing...");
+            try
+            {
+                var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                using (var img = new MagickImage(Path.Combine(dir, "iconmonstr-redo-3-16.png")))
+                {
+                    var w = img.Width;
+                    Console.WriteLine(img.Width);
+                    Console.WriteLine(img.Height);
+                }
+                Console.WriteLine("Done!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.Write("Press any key to exit...");
+            Console.ReadKey(true);
         }
     }
 }
